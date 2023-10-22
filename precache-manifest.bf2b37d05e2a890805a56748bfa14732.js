9044a3a291306408927995b36a860be4fd471020
Irmina González <escallobro@gmail.com>
2023-10-22 21:17:53 +0100

    Updates

---
+++ precache-manifest.bf2b37d05e2a890805a56748bfa14732.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e19f0fae07c415956cb3979d28735fd",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "b20a5dc78ae70b0bebcf",
+    "url": "/codepen-clone/static/css/2.7c584778.chunk.css"
+  },
+  {
+    "revision": "1e4185ee83357c4e5e49",
+    "url": "/codepen-clone/static/css/main.0dbb3662.chunk.css"
+  },
+  {
+    "revision": "b20a5dc78ae70b0bebcf",
+    "url": "/codepen-clone/static/js/2.ea8c43c7.chunk.js"
+  },
+  {
+    "revision": "f75561adb7f4db3b610232e7231a7ed4",
+    "url": "/codepen-clone/static/js/2.ea8c43c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e4185ee83357c4e5e49",
+    "url": "/codepen-clone/static/js/main.8b771c19.chunk.js"
+  },
+  {
+    "revision": "e2a47c0fc7a7abcff4eb",
+    "url": "/codepen-clone/static/js/runtime-main.30e586a1.js"
+  }
+]);
